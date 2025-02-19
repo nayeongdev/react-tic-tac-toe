922f7401153bccad971948d60c31f8d378d7a3c5
nayeongdev <nayeongkim.dev@gmail.com>
2025-02-20 00:35:59 +0900

    다중 리스트 렌더링링

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
 
 function App() {
   return (
@@ -8,9 +9,10 @@ function App() {
           <Player initialName="Player 1" symbol="X" />
           <Player initialName="Player 2" symbol="O" />
         </ol>
+        <GameBoard />
       </div>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
